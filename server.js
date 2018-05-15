5299782304879b7891ee0ade11547930367bf8e0
Mathias <you@example.com>
2018-05-15 13:03:14 +0200

    Added projects Route

--- server.js
+++ server.js
@@ -41,6 +41,13 @@ app.get("/", (req, res) => {
   })
 });
 
+app.get("/projects", (req, res) => {
+  res.render("projects.hbs", {
+    pageTitle: "Projects",
+    welcomeMessage: "Here you can see my completed projects"
+  })
+});
+
 app.get("/about", (req, res) => {
   res.render("about.hbs", {
     pageTitle: "About Page",
